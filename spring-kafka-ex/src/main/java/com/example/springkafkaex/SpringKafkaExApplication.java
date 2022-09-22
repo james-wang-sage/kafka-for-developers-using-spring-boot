1655107e28977980d6a1bcb709b9405ba2f186e7
James Wang <james.wang@sage.com>
2022-09-22 14:46:10 -0700

    created a new ex project

---
+++ spring-kafka-ex/src/main/java/com/example/springkafkaex/SpringKafkaExApplication.java
@@ -0,0 +1,13 @@
+package com.example.springkafkaex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaExApplication.class, args);
+	}
+
+}
---
+++ spring-kafka-ex/src/test/java/com/example/springkafkaex/SpringKafkaExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springkafkaex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
